a613546633096b43904883114e063bda2865d278
Rogon101 <33530022+Rogon101@users.noreply.github.com>
2020-05-08 20:11:25 -0500

    initial commit 3

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost/info3180_project_1" #Change for testing
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+app.config['UPLOAD_FOLDER'] = './app/static/uploads'
+
+db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
+
+#flask login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config.from_object(__name__)
+
+from app import views
