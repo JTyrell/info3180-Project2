61517ac362a1badb77fdf9309f3b1de25a3c7661
Rogon101 <33530022+Rogon101@users.noreply.github.com>
2020-05-13 18:09:49 -0500

    api added

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\x
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost/info3180_project_1" #Change for testing
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
+app.config['RETRIEVED_FILE'] = './app/static/uploads'
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
